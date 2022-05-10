01218389d3645dd9c3daa5a54bb3918b211dc058
prkhrv <prakhar_varshney@outlook.com>
2022-05-10 01:35:34 -0400

    added code

--- face_recognition_2_factor_auth/settings.py
+++ face_recognition_2_factor_auth/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from re import TEMPLATE
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
